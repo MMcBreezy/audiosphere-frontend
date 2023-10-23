1162389801fa2eee4ef02eff738f0c0f328147fa
Michael McBride <michaelmcbride@Michaels-MacBook-Air.local>
2023-10-23 18:41:44 -0400

    feed and posts styles/tests

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,22 @@
 import React from "react";
-import "./styles.css";
+import profilePic from "../assets/4299915.png";
+import "./post.css";
 
 const Post = () => {
   return (
-    <div className="post-container">Post Content</div>
+    <div className="post-container">
+      <div className="user-section">
+        <img
+          src={profilePic}
+          alt="profile-pic"
+          className="profile-image"
+        />
+        <div className="name">User</div>
+      </div>
+      <div className="content-section">Content</div>
+      <div className="buttons-section">Buttons</div>
+      <div className="comments-section">Comments</div>
+    </div>
   );
 };
 
