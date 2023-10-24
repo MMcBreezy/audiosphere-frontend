8463cb22ae95fe5071dce633a30dfdcff5c942ef
Michael McBride <michaelmcbride@Michaels-MacBook-Air.local>
2023-10-24 12:27:22 -0400

    initial side content update

--- src/components/SideContent.js
+++ src/components/SideContent.js
@@ -1,10 +1,46 @@
 import React from "react";
+import profilePic from "../assets/4299915.png";
 import "./sideContent.css";
 
 const SideContent = () => {
   return (
     <div className="side-container">
-      <div>Side Content</div>
+      <div className="greeting">
+        Hi <span className="userName">User</span>,
+        <p className="greeting-message">
+          Here is some content related to your account!
+        </p>
+      </div>
+      <div className="suggest-follow-section">
+        Suggested artists:
+        <div className="profiles">
+          <img
+            src={profilePic}
+            alt="profile-pic"
+            className="suggest-follow-profile-image"
+          />
+          <img
+            src={profilePic}
+            alt="profile-pic"
+            className="suggest-follow-profile-image"
+          />
+          <img
+            src={profilePic}
+            alt="profile-pic"
+            className="suggest-follow-profile-image"
+          />
+          <img
+            src={profilePic}
+            alt="profile-pic"
+            className="suggest-follow-profile-image"
+          />
+          <img
+            src={profilePic}
+            alt="profile-pic"
+            className="suggest-follow-profile-image"
+          />
+        </div>
+      </div>
     </div>
   );
 };
